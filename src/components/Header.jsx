ad010cc5b8611c0edd25ca68a47ccc096d5bb3c2
jaydipchangani <pjaydip956@gmail.com>
2025-08-27 18:21:44 +0530

    simple header component complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div>
+        <div class="flex justify-around bg-blue-300 py-3">
+          <h2 class="text-3xl font-bold  ">Logo</h2>
+          <h2 class="text-3xl font-bold ">Daily Journal</h2>
+
+          <button class="">Dark/Light</button>
+        </div>
+      </div>
+    </>
+  );
 }
