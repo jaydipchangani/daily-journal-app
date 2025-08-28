374f410b90c9260defd8b2326ebcd395cfc824b6
jaydipchangani <pjaydip956@gmail.com>
2025-08-28 11:19:12 +0530

    add, edit and delete task complete

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Body() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+
+  function addOrUpdateTask() {
+    if (task.trim() === "") return;
+
+    if (editIndex !== null) {
+      const updatedList = [...taskList];
+      updatedList[editIndex] = task;
+      setTaskList(updatedList);
+      setEditIndex(null);
+      setTask("");
+    } else {
+      setTaskList([...taskList, task]);
+      setTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    const updatedtask = taskList.filter((_, i) => i !== index);
+    setTaskList(updatedtask);
+  }
+
+  function editTask(index) {
+    setTask(taskList[index]);
+    setEditIndex(index);
+  }
+
   return (
     <div>
       <h1 class="text-3xl font-bolder underline">Body</h1>
+      <label name="task">Enter your task:</label>{" "}
+      <input
+        type="text"
+        className="border-2"
+        name="task"
+        placeholder="Enter a task"
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+      />
+      <button onClick={addOrUpdateTask} class="border-2 ml-2 px-1">
+        {editIndex == null ? "Add Task" : "Edit Task"}
+      </button>
+      <ul>
+        {taskList.map((t, index) => (
+          <li key={index}>
+            {t}
+            <button
+              onClick={() => editTask(index)}
+              className="border-2 px-1 ml-2"
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => deleteTask(index)}
+              className="border-2 px-1 ml-2"
+            >
+              Complete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
